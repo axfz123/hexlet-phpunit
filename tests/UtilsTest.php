e50bd331131057e222c1906754a89588c853d5da
Alexey Zhabin <axfz@ya.ru>
2025-02-20 21:59:03 +0300

    Add reversed file string test

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -9,14 +9,21 @@ use function Hexlet\Phpunit\Utils\reverseString;
 // Имя класса совпадает с именем файла
 class UtilsTest extends TestCase
 {
-    // Метод (функция), определенный внутри класса,
-    // Должен начинаться со слова test
-    // Ключевое слово public нужно, чтобы PHPUnit мог вызвать этот тест снаружи
+    public function getFixtureFullPath($fixtureName)
+    {
+        $parts = [__DIR__, 'fixtures', $fixtureName];
+        return realpath(implode('/', $parts));
+    }
+
     public function testReverse(): void
     {
-        // Сначала идет ожидаемое значение (expected)
-        // И только потом актуальное (actual)
         $this->assertEquals('', reverseString(''));
         $this->assertEquals('olleh', reverseString('hello'));
+    }
+
+    public function testReverseFile(): void
+    {
+        $text = file_get_contents($this->getFixtureFullPath('someText.txt'));
+        $this->assertStringEqualsFile($this->getFixtureFullPath('someTextReversed.txt'), reverseString($text));
     }
 }
